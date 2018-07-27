99e4ce6bb0a5b78c475ce9294e90c02e3d33baf2
fuookami <fuookami@163.com>
2018-07-27 23:43:15 +0800

    搭建竞赛信息类解析、格式化器框架

---
+++ CompetitionConfigurationModule/CompetitionConfigurationAnalyzer.cs
@@ -0,0 +1,79 @@
+﻿using System;
+
+namespace CompetitionConfigurationModule
+{
+    class CompetitionConfigurationAnalyzer
+    {
+        public enum InputType
+        {
+            File,
+            Binary
+        }
+        
+        public enum ErrorCode
+        {
+            NoError
+        }
+
+        private InputType inputType;
+        private ErrorCode lastErrorCode;
+        private String lastError;
+        private CompetitionInfo result;
+
+        public InputType DataInputType
+        {
+            get { return inputType; }
+            set { inputType = value; }
+        }
+
+        public ErrorCode LastErrorCode
+        {
+            get { return lastErrorCode; }
+        }
+
+        public String LastError
+        {
+            get { return lastError; }
+        }
+
+        public CompetitionConfigurationAnalyzer(InputType dataInputType = InputType.File)
+        {
+            inputType = dataInputType;
+            lastErrorCode = ErrorCode.NoError;
+            result = null;
+        }
+
+        public bool Analyze(String data)
+        {
+            switch (inputType)
+            {
+                case InputType.Binary:
+                    return AnalyzeFromBinary(data);
+                case InputType.File:
+                    return AnalyzeFromFile(data);
+                default:
+                    return false;
+            }
+        }
+
+        private bool AnalyzeFromFile(String url)
+        {
+            // load from the file
+            String data = "1";
+            return AnalyzeFromBinary(data);
+        }
+
+        private bool AnalyzeFromBinary(String json)
+        {
+            // to do
+            result = new CompetitionInfo();
+            return true;
+        }
+
+        private void RefreshError(ErrorCode code, String text)
+        {
+            lastErrorCode = code;
+            lastError = String.Format("error {0}({1}): {2}", (int)code, code.ToString(), text);
+        }
+    }
+}
---
+++ CompetitionConfigurationModule/CompetitionConfigurationNormalizer.cs
@@ -0,0 +1,81 @@
+﻿using System;
+
+namespace CompetitionConfigurationModule
+{
+    class CompetitionConfigurationNormalizer
+    {
+        public enum ErrorCode
+        {
+            NoError
+        }
+
+        private ErrorCode lastErrorCode;
+        private String lastError;
+        private CompetitionInfo outputData;
+        private String binaryData;
+
+        public ErrorCode LastErrorCode
+        {
+            get { return lastErrorCode; }
+        }
+
+        public String LastError
+        {
+            get { return lastError; }
+        }
+
+        public CompetitionInfo Data
+        {
+            get { return outputData; }
+            set
+            {
+                outputData = value;
+                binaryData = "";
+            }
+        }
+
+        public String Binary
+        {
+            get { return binaryData; }
+        }
+
+        public CompetitionConfigurationNormalizer(CompetitionInfo data)
+        {
+            lastErrorCode = ErrorCode.NoError;
+            outputData = data;
+            binaryData = "";
+        }
+
+        private bool NormalizeToBinary()
+        {
+            // to do
+            binaryData = "test";
+            return true;
+        }
+
+        public bool Normalize()
+        {
+            if (binaryData.Length != 0)
+            {
+                return true;
+            }
+            return NormalizeToBinary();
+        }
+
+        public bool NormalizeToFile(String url)
+        {
+            if (binaryData.Length == 0 && !NormalizeToBinary())
+            {
+                return false;
+            }
+            // save to file
+            return true;
+        }
+
+        private void RefreshError(ErrorCode code, String text)
+        {
+            lastErrorCode = code;
+            lastError = String.Format("error {0}({1}): {2}", (int)code, code.ToString(), text);
+        }
+    }
+}
