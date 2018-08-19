f7275ad0a10e3e0157a03a4d445865ba1abe91ed
fuookami <fuookami@163.com>
2018-08-19 22:15:29 +0800

    添加并实现道次信息

--- CompetitionConfigurationModule/CompetitionInfo.cs
+++ CompetitionConfigurationModule/CompetitionInfo.cs
@@ -25,6 +25,10 @@ namespace SCAS
             private TeamCategoryPool teamCategories;
             private TeamInfoPool teamInfos;
 
+            private UInt32 displayBeginLine;
+            private UInt32 numberOfDisplayLines;
+            private List<UInt32> useLines;
+
             private List<EventInfo> eventInfos;
             private Dictionary<Session, GameInfoList> gameInfos;
 
@@ -124,6 +128,49 @@ namespace SCAS
             {
                 get { return teamInfos; }
             }
+
+            public UInt32 DisplayBeginLine
+            {
+                get { return displayBeginLine; }
+            }
+
+            public UInt32 NumberOfDisplayLines
+            {
+                get { return numberOfDisplayLines; }
+            }
+
+            public UInt32 UseBeginLine
+            {
+                get { return useLines[0]; }
+            }
+
+            public UInt32 NumberOfUseLines
+            {
+                get { return (UInt32)useLines.Count; }
+            }
+
+            public List<UInt32> UseLines
+            {
+                get { return useLines; }
+                set
+                {
+                    UInt32 maxLine = displayBeginLine + numberOfDisplayLines;
+                    if (value == null || value.Count == 0)
+                    {
+                        throw new Exception("传入的使用道次信息是个非法值");
+                    }
+                    value.Sort();
+                    foreach (UInt32 line in value)
+                    {
+                        if (line > maxLine)
+                        {
+                            throw new Exception(String.Format("有非法的道次值{0}", line));
+                        }
+                    }
+                    useLines = value;
+                }
+            }
+
             public List<EventInfo> EventInfos
             {
                 get { return eventInfos; }
@@ -152,6 +199,8 @@ namespace SCAS
                 teamCategories = new TeamCategoryPool();
                 teamInfos = new TeamInfoPool();
 
+                SetLineConfiguration();
+
                 gameInfos = new Dictionary<Session, GameInfoList>();
                 eventInfos = new List<EventInfo>();
             }
@@ -159,6 +208,45 @@ namespace SCAS
             public EventInfo GenerateNewEventInfo()
             {
                 return new EventInfo(this);
+            }
+
+            public bool SetLineConfiguration(UInt32 useBeginLine = 1, UInt32 numberOfUseLines = 8)
+            {
+                return SetLineConfiguration(useBeginLine, numberOfUseLines, useBeginLine, numberOfUseLines);
+            }
+
+            public bool SetLineConfiguration(UInt32 useBeginLine, UInt32 numberOfUseLines, UInt32 displayBeginLine, UInt32 numberOfDisplayLines)
+            {
+                if (numberOfUseLines == 0 || numberOfDisplayLines == 0 || (useBeginLine < displayBeginLine)
+                    || ((useBeginLine + numberOfUseLines) > (displayBeginLine + NumberOfDisplayLines)))
+                {
+                    return false;
+                }
+
+                this.displayBeginLine = displayBeginLine;
+                this.numberOfDisplayLines = numberOfDisplayLines;
+
+                useLines.Clear();
+                for (UInt32 i = useBeginLine; i != numberOfUseLines; ++i)
+                {
+                    useLines.Add(displayBeginLine + i);
+                }
+
+                return true;
+            }
+
+            public bool SetLineConfiguration(List<UInt32> useLines, UInt32 displayBeginLine, UInt32 numberOfDisplayLines)
+            {
+                if (numberOfDisplayLines == 0)
+                {
+                    return false;
+                }
+
+                this.displayBeginLine = displayBeginLine;
+                this.numberOfDisplayLines = numberOfDisplayLines;
+                this.UseLines = useLines;
+
+                return true;
             }
         }
     };
