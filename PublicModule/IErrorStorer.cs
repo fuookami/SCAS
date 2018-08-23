e2ae61ee01e2fd5e36f4174e1d4ad56f7e54d20a
fuookami <fuookami@163.com>
2018-08-24 00:50:32 +0800

    v0.01.001：实现错误存储类

---
+++ PublicModule/IErrorStorer.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace SCAS
+{
+    public enum ErrorCode
+    {
+        None
+    };
+
+    public class ErrorStorer
+    {
+        private ErrorCode lastErrorCode;
+        private String lastError;
+
+        public ErrorCode LastErrorCode
+        {
+            get { return lastErrorCode; }
+        }
+
+        public String LastError
+        {
+            get { return lastError; }
+        }
+
+        public ErrorStorer()
+        {
+            lastErrorCode = ErrorCode.None;
+        }
+
+        public void Refresh()
+        {
+            RefreshError(ErrorCode.None);
+        }
+
+        protected void RefreshError(ErrorCode code, String text = "")
+        {
+            lastErrorCode = code;
+            lastError = String.Format("error {0}({1}): {2}", (int)code, code.ToString(), text);
+        }
+    };
+};
