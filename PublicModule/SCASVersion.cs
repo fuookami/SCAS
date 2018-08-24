9b7ea692002081fc8662bb6b66158920ccb2567e
fuookami <fuookami@163.com>
2018-08-25 05:28:30 +0800

    v0.01.004：在公共模块新增SCAS版本号

---
+++ PublicModule/SCASVersion.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace SCAS
+{
+    public class Version
+    {
+        const Int32 MajorVersion = 0;
+        const Int32 SubVersion = 1;
+        const Int32 ModifyVersion = 4;
+
+        new static String ToString()
+        {
+            return String.Format("{0}{1:D2}{2:D3}", MajorVersion, SubVersion, ModifyVersion);
+        }
+    }
+}
