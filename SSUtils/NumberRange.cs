fe74fb5f593a8f659ba826666cc6ce6680a199c0
fuookami <fuookami@163.com>
2018-08-26 23:44:49 +0800

    v0.01.007：重构公共模块部分的组织结构

---
+++ SSUtils/NumberRange.cs
@@ -0,0 +1,74 @@
+﻿using System;
+
+namespace SSUtils
+{
+    public class NumberRange : UInt32Range
+    {
+        public const UInt32 Nolimit = 0;
+
+        public NumberRange(UInt32 min = 0, UInt32 max = Nolimit)
+            : base(min, max)
+        {
+        }
+
+        public bool Set(UInt32 number)
+        {
+            return Set(number, number);
+        }
+
+        public override bool Valid()
+        {
+            return Valid(Minimun, Maximun);
+        }
+
+        protected override bool Valid(UInt32 min, UInt32 max)
+        {
+            return min == 0
+                || min <= max;
+        }
+    }
+
+    public class EnabledNumberRange
+    {
+        private NumberRange _range;
+        
+        public NumberRange Range
+        {
+            get
+            {
+                return _range;
+            }
+            set
+            {
+                SetEnabled(value);
+            }
+        }
+
+        public EnabledNumberRange()
+        {
+            SetDisabled();
+        }
+
+        public bool IsEnabled()
+        {
+            return _range != null;
+        }
+
+        public bool SetEnabled()
+        {
+            return SetEnabled(new NumberRange());
+        }
+
+        public bool SetEnabled(NumberRange range)
+        {
+            _range = range ?? throw new Exception("设置的范围值是个无效值");
+            return true;
+        }
+
+        public bool SetDisabled()
+        {
+            _range = null;
+            return true;
+        }
+    }
+}
---
+++ SSUtils/Order.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace SSUtils
+{
+    public class Order : IComparable
+    {
+        public const Int32 NotSet = -1;
+
+        private Int32 _order;
+
+        public Int32 Value
+        {
+            get
+            {
+                return _order;
+            }
+            set
+            {
+                _order = value >= 0 ? value : NotSet;
+            }
+        }
+
+        public Order(Int32 order = NotSet)
+        {
+            _order = order;
+        }
+
+        public bool Valid()
+        {
+            return Value != NotSet;
+        }
+
+        public int CompareTo(object obj)
+        {
+            Order rhs = (Order)obj;
+            return Valid() && rhs.Valid() ? Value.CompareTo(rhs.Value)
+                : Valid() ? Int32.MaxValue 
+                : rhs.Valid() ? Int32.MinValue : 0;
+        }
+    };
+};
