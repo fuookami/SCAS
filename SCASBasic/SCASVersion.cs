899a4b702e8b1d3f81bfd5e9940eac2b807b00ac
fuookami <fuookami@163.com>
2018-10-28 02:24:33 +0800

    v0.04.012：移除前后组的跳转

--- SCASBasic/SCASVersion.cs
+++ SCASBasic/SCASVersion.cs
@@ -6,7 +6,7 @@ namespace SCAS
     {
         const Int32 MajorVersion = 0;
         const Int32 SubVersion = 4;
-        const Int32 ModifyVersion = 11;
+        const Int32 ModifyVersion = 12;
 
         new static String ToString()
         {
