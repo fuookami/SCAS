52710dc3bd95d7e784d67920116cb9fccde39de0
fuookami <fuookami@163.com>
2020-02-02 22:27:56 +0800

    feature: 实现用户聚合根。

---
+++ Domain/UniversalComponent/Utils/Encryptor.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace SCAS.Utils
+{
+    public interface IEncryptor
+    {
+        public string Encrypt(string raw);
+        public string Decrypt(string raw);
+    }
+}
--- Domain/UserContext/Model/User/User.cs
+++ Domain/UserContext/Model/User/User.cs
@@ -1,19 +1,62 @@
-﻿using System.Collections.Generic;
+﻿using SCAS.Utils;
+using System.Diagnostics.CodeAnalysis;
 
 namespace SCAS.Domain.UserContext
 {
-    public 
+    public class UserID
+        : DomainEntityID
+    {
+        public override string ToString()
+        {
+            return string.Format("User-{0}", ID);
+        }
+    }
+
+    public class UserValue
+        : DomainEntityValueBase
+    {
+        [NotNull] public string PersonID { get; internal set; }
+        [NotNull] public string Account { get; internal set; }
+        [NotNull] public string Password { get; internal set; }
+        public bool Available { get; internal set; }
+    }
 
     public class User
+        : DomainAggregateRoot<UserValue, UserID>
     {
-        // 使用者
-        public Person Person { get; internal set; }
+        private IEncryptor encryptor;
 
-        // 密码
-        public string Password { get; internal set; }
+        public Person Person { get; }
+        public string Account { get; }
+        public string Password { get; }
+        public bool Available { get; }
 
-        // 关联的账号
-        internal List<Account> RelatedAccountsList { get; set; }
-        public IReadOnlyList<Account> RelatedAccounts { get { return RelatedAccountsList; }  }
+        internal User(Person person, string account, string password, IEncryptor encryptor = null)
+        {
+            Person = person;
+            Account = encryptor?.Decrypt(account) ?? account;
+            Password = encryptor?.Decrypt(password) ?? password;
+            Available = true;
+        }
+
+        internal User(UserID id, Person person, string account, string password, bool available, IEncryptor encryptor = null)
+            : base(id)
+        {
+            Person = person;
+            Account = encryptor?.Decrypt(account) ?? account;
+            Password = encryptor?.Decrypt(password) ?? password;
+            Available = available;
+        }
+
+        public override UserValue ToValue()
+        {
+            return base.ToValue(new UserValue
+            {
+                PersonID = this.Person.ID, 
+                Account = this.encryptor?.Encrypt(Account) ?? Account, 
+                Password = this.encryptor?.Encrypt(Password) ?? Password, 
+                Available = this.Available
+            });
+        }
     }
 }
