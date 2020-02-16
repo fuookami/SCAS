8b6257314259759c4f5a07c9a55d1209e20d58af
fuookami <fuookami@163.com>
2020-02-17 00:41:54 +0800

    feature: 实现用户注册事件。

--- Domain/UserContext/Event/OrganizationRegisterInitiatationEvent.cs
+++ Domain/UserContext/Event/OrganizationRegisterInitiatationEvent.cs
@@ -7,6 +7,7 @@ namespace SCAS.Domain.UserContext
     public struct OrganizationRegisterInitiatationEventData
     {
         public string ID { get; }
+        public string SID { get; }
         public string OrgID { get; }
         public string RegionID { get; }
         public string InitiatorID { get; }
@@ -15,6 +16,7 @@ namespace SCAS.Domain.UserContext
         internal OrganizationRegisterInitiatationEventData(OrganizationRegisterForm form)
         {
             ID = form.ID;
+            SID = form.SID;
             OrgID = form.Org.ID;
             RegionID = form.RegisteredRegion.ID;
             InitiatorID = form.Info.Initiator.ID;
@@ -27,7 +29,7 @@ namespace SCAS.Domain.UserContext
     {
         [NotNull] private OrganizationRegisterForm form;
 
-        public override string Message { get { return string.Format("Register of organization {0} to region {1} initiated by {2}, code {3}", form.Org.Info.Name, form.RegisteredRegion.Info.Name, form.Info.Initiator.Info.Name, form.Info.PrefixCode); } }
+        public override string Message { get { return string.Format("Register of organization {0} to region {1} initiated by {2}, prefix code {3}", form.Org.Info.Name, form.RegisteredRegion.Info.Name, form.Info.Initiator.Info.Name, form.Info.PrefixCode); } }
 
         internal OrganizationRegisterInitiatationEvent(OrganizationRegisterForm newForm, IExtractor extractor = null)
             : base((uint)SCASEvent.OrganizationInfoModified, (uint)SCASEventType.Model, (uint)SCASEventLevel.Common, (uint)SCASEventPriority.Common, new OrganizationRegisterInitiatationEventData(newForm), extractor)
---
+++ Domain/UserContext/Event/PersonRegisterInitiatationEvent.cs
@@ -0,0 +1,54 @@
+﻿using SCAS.Module;
+using System.Diagnostics.CodeAnalysis;
+
+namespace SCAS.Domain.UserContext
+{
+    public struct PersonRegisterInitiatationEventData
+    {
+        public string ID { get; }
+        public string SID { get; }
+        public string PersonID { get; }
+        public string RegionID { get; }
+        public string InitiatorID { get; }
+        public string OrgID { get; }
+
+        internal PersonRegisterInitiatationEventData(PersonRegisterForm form)
+        {
+            ID = form.ID;
+            SID = form.SID;
+            PersonID = form.Person.ID;
+            RegionID = form.RegisteredRegion.ID;
+            InitiatorID = form.Info.Initiator.ID;
+            OrgID = form.BelongingOrganization?.ID;
+        }
+    }
+
+    public class PersonRegisterInitiatationEvent
+        : DomainEventBase<DomainEventValue, PersonRegisterInitiatationEventData>
+    {
+        [NotNull] private PersonRegisterForm form;
+
+        public override string Message { get { return GetMessage(); } }
+
+        internal PersonRegisterInitiatationEvent(PersonRegisterForm newForm, IExtractor extractor = null)
+            : base((uint)SCASEvent.OrganizationInfoModified, (uint)SCASEventType.Model, (uint)SCASEventLevel.Common, (uint)SCASEventPriority.Common, new PersonRegisterInitiatationEventData(newForm), extractor)
+        {
+            form = newForm;
+        }
+
+        private string GetMessage()
+        {
+            var ret = string.Format("Register of person {0} to region {1} initiated by {2}", form.Person.Info.Name, form.RegisteredRegion.Info.Name, form.Info.Initiator.Info.Name);
+            if (form.BelongingOrganization != null)
+            {
+                ret += string.Format(", belonging organization {0}", form.BelongingOrganization.Info.Name);
+            }
+            return ret;
+        }
+
+        public override DomainEventValue ToValue()
+        {
+            return base.ToValue(new DomainEventValue { });
+        }
+    }
+}
