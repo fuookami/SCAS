94dbf0dacbda60fd303a96deb15e94eceb84be49
fuookami <fuookami@163.com>
2020-02-21 00:44:56 +0800

    feature: 实现组织注册归档事件。

--- Domain/UserContext/Event/OrganizationRegister/OrganizationRegisterArchivedEvent.cs
+++ Domain/UserContext/Event/OrganizationRegister/OrganizationRegisterArchivedEvent.cs
@@ -2,7 +2,41 @@
 
 namespace SCAS.Domain.UserContext
 {
-    class OrganizationRegisterArchivedEvent
+    public struct OrganizationRegisterArchivedEventData
     {
+        public string ID { get; }
+
+        internal OrganizationRegisterArchivedEventData(OrganizationRegister register)
+        {
+            ID = register.ID;
+        }
+    }
+
+    public class OrganizationRegisterArchivedEvent
+        : DomainEventBase<DomainEventValue, OrganizationRegisterArchivedEventData>
+    {
+        private OrganizationRegister register;
+
+        public override string Message => string.Format("Register of organization {0} to region {1} archived.", register.RegisteredRegion.Info.Name, register.Org.Info.Name);
+
+        private OrganizationRegisterArchivedEvent(IDomainEvent trigger, OrganizationRegister targetRegister, IExtractor extractor)
+            : base(trigger, (uint)SCASEvent.OrganizationRegisterArchived, (uint)SCASEventType.Model, (uint)SCASEventLevel.Common, (uint)SCASEventPriority.Common, new OrganizationRegisterArchivedEventData(targetRegister), extractor)
+        {
+            register = targetRegister;
+        }
+
+        internal OrganizationRegisterArchivedEvent(OrganizationRegister targetRegister, IExtractor extractor = null)
+            : this((IDomainEvent)null, targetRegister, extractor) { }
+
+        internal OrganizationRegisterArchivedEvent(OrganizationArchivedEvent trigger, OrganizationRegister targetRegister, IExtractor extractor = null)
+            : this((IDomainEvent)trigger, targetRegister, extractor) { }
+
+        internal OrganizationRegisterArchivedEvent(RegionArchivedEvent trigger, OrganizationRegister targetRegister, IExtractor extractor = null)
+            : this((IDomainEvent)trigger, targetRegister, extractor) { }
+
+        public override DomainEventValue ToValue()
+        {
+            return base.ToValue(new DomainEventValue { });
+        }
     }
 }
