da0555012611d76d71c12fd528debc69ff1d12de
fuookami <fuookami@163.com>
2020-02-21 00:35:07 +0800

    feature: 实现组织归档事件。

--- Domain/UserContext/Event/Organization/OrganizationArchivedEvent.cs
+++ Domain/UserContext/Event/Organization/OrganizationArchivedEvent.cs
@@ -2,7 +2,32 @@
 
 namespace SCAS.Domain.UserContext
 {
-    class OrganizationArchivedEvent
+    public struct OrganizationArchivedEventData
     {
+        public string ID { get; }
+
+        internal OrganizationArchivedEventData(Organization org)
+        {
+            ID = org.ID;
+        }
+    }
+
+    public class OrganizationArchivedEvent
+        : DomainEventBase<DomainEventValue, OrganizationArchivedEventData>
+    {
+        private Organization org;
+
+        public override string Message => string.Format("Organization {0} archived.", org.Info.Name);
+
+        internal OrganizationArchivedEvent(Organization targetOrg, IExtractor extractor = null)
+            : base((uint)SCASEvent.OrganizationArchived, (uint)SCASEventType.Model, (uint)SCASEventLevel.Common, (uint)SCASEventPriority.Common, new OrganizationArchivedEventData(targetOrg), extractor)
+        {
+            org = targetOrg;
+        }
+
+        public override DomainEventValue ToValue()
+        {
+            return base.ToValue(new DomainEventValue { });
+        }
     }
 }
