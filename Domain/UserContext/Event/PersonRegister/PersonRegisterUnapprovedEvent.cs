4b1ed35dd23f6fe4ce39dd403e4c32e307787eb7
fuookami <fuookami@163.com>
2020-02-22 01:37:22 +0800

    feature: 实现个人注册事件不通过事件。

--- Domain/UserContext/Event/PersonRegister/PersonRegisterUnapprovedEvent.cs
+++ Domain/UserContext/Event/PersonRegister/PersonRegisterUnapprovedEvent.cs
@@ -1,8 +1,48 @@
 ﻿using SCAS.Module;
+using System.Diagnostics.CodeAnalysis;
 
 namespace SCAS.Domain.UserContext
 {
-    class PersonRegisterUnapprovedEvent
+    public struct PersonRegisterUnapprovedEventData
     {
+        public string ID { get; }
+        public string ExaminerID { get; }
+        public string Annotation { get; }
+
+        internal PersonRegisterUnapprovedEventData(PersonRegisterForm form)
+        {
+            ID = form.ID;
+            ExaminerID = form.Examination.Examiner.ID;
+            Annotation = form.Examination.Annotation;
+        }
+    }
+
+    public class PersonRegisterUnapprovedEvent
+        : DomainEventBase<DomainEventValue, PersonRegisterUnapprovedEventData>
+    {
+        [NotNull] private PersonRegisterForm form;
+
+        public override string Message => GetMessage();
+
+        internal PersonRegisterUnapprovedEvent(PersonRegisterForm targetForm, IExtractor extractor = null)
+            : base((uint)SCASEvent.PersonRegisterUnapproved, (uint)SCASEventType.Model, (uint)SCASEventLevel.Common, (uint)SCASEventPriority.Common, new PersonRegisterUnapprovedEventData(targetForm), extractor)
+        {
+            form = targetForm;
+        }
+
+        private string GetMessage()
+        {
+            var ret = string.Format("Register of person {0} to region {1} Unapproved by {2}", form.Person.Info.Name, form.RegisteredRegion.Info.Name, form.Examination.Examiner.Info.Name);
+            if (form.BelongingOrganization != null)
+            {
+                ret += string.Format(", belonging organization {0}", form.BelongingOrganization.Info.Name);
+            }
+            return ret;
+        }
+
+        public override DomainEventValue ToValue()
+        {
+            return base.ToValue(new DomainEventValue { });
+        }
     }
 }
