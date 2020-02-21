7d78804c24c4581e79112205f827592a36158b0e
fuookami <fuookami@163.com>
2020-02-22 01:46:32 +0800

    feature: 实现个人注册事件。

--- Domain/UserContext/Event/PersonRegister/PersonRegisteredEvent.cs
+++ Domain/UserContext/Event/PersonRegister/PersonRegisteredEvent.cs
@@ -3,7 +3,50 @@ using System.Diagnostics.CodeAnalysis;
 
 namespace SCAS.Domain.UserContext
 {
-    class PersonRegisteredEvent
+    public struct PersonRegisteredEventData
     {
+        public string ID { get; }
+        public string SID { get; }
+        public string PersonID { get; }
+        public string RegionID { get; }
+        public string OrgID { get; }
+
+        internal PersonRegisteredEventData(PersonRegister register)
+        {
+            ID = register.ID;
+            SID = register.SID;
+            PersonID = register.Person.ID;
+            RegionID = register.RegisteredRegion.ID;
+            OrgID = register.BelongingOrganization?.ID;
+        }
+    }
+
+    public class PersonRegisteredEvent
+        : DomainEventBase<DomainEventValue, PersonRegisteredEventData>
+    {
+        [NotNull] private PersonRegister register;
+
+        public override string Message => GetMessage();
+
+        internal PersonRegisteredEvent(PersonRegisterApprovedEvent trigger, PersonRegister targetRegister, IExtractor extractor = null)
+            : base(trigger, (uint)SCASEvent.PersonRegistered, (uint)SCASEventType.Model, (uint)SCASEventLevel.Common, (uint)SCASEventPriority.Common, new PersonRegisteredEventData(targetRegister), extractor)
+        {
+            register = targetRegister;
+        }
+
+        private string GetMessage()
+        {
+            var ret = string.Format("Person {0} registered to region {1}", register.Person.Info.Name, register.RegisteredRegion.Info.Name);
+            if (register.BelongingOrganization != null)
+            {
+                ret += string.Format(", belonging organization {0}", register.BelongingOrganization.Info.Name);
+            }
+            return ret;
+        }
+
+        public override DomainEventValue ToValue()
+        {
+            return base.ToValue(new DomainEventValue { });
+        }
     }
 }
