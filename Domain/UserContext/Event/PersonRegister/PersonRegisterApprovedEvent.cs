8d2d2cde2d7a6b8dffce827f2ee5c3fb7e7a988d
fuookami <fuookami@163.com>
2020-02-22 01:36:19 +0800

    feature: 实现用户注册通过事件。

--- Domain/UserContext/Event/PersonRegister/PersonRegisterApprovedEvent.cs
+++ Domain/UserContext/Event/PersonRegister/PersonRegisterApprovedEvent.cs
@@ -1,8 +1,48 @@
 ﻿using SCAS.Module;
+using System.Diagnostics.CodeAnalysis;
 
 namespace SCAS.Domain.UserContext
 {
-    class PersonRegisterApprovedEvent
+    public struct PersonRegisterApprovedEventData
     {
+        public string ID { get; }
+        public string ExaminerID { get; }
+        public string Annotation { get; }
+
+        internal PersonRegisterApprovedEventData(PersonRegisterForm form)
+        {
+            ID = form.ID;
+            ExaminerID = form.Examination.Examiner.ID;
+            Annotation = form.Examination.Annotation;
+        }
+    }
+
+    public class PersonRegisterApprovedEvent
+        : DomainEventBase<DomainEventValue, PersonRegisterApprovedEventData>
+    {
+        [NotNull] private PersonRegisterForm form;
+
+        public override string Message => GetMessage();
+
+        internal PersonRegisterApprovedEvent(PersonRegisterForm targetForm, IExtractor extractor = null)
+            : base((uint)SCASEvent.PersonRegisterApproved, (uint)SCASEventType.Model, (uint)SCASEventLevel.Common, (uint)SCASEventPriority.Common, new PersonRegisterApprovedEventData(targetForm), extractor)
+        {
+            form = targetForm;
+        }
+
+        private string GetMessage()
+        {
+            var ret = string.Format("Register of person {0} to region {1} approved by {2}", form.Person.Info.Name, form.RegisteredRegion.Info.Name, form.Examination.Examiner.Info.Name);
+            if (form.BelongingOrganization != null)
+            {
+                ret += string.Format(", belonging organization {0}", form.BelongingOrganization.Info.Name);
+            }
+            return ret;
+        }
+
+        public override DomainEventValue ToValue()
+        {
+            return base.ToValue(new DomainEventValue { });
+        }
     }
 }
