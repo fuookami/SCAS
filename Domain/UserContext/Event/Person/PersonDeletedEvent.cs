a163b633f05049c8c91be2f3b4e8c37400c7c5f8
fuookami <fuookami@163.com>
2020-02-29 17:06:26 +0800

    feature: 实现个人删除事件。

---
+++ Domain/UserContext/Event/Person/PersonDeletedEvent.cs
@@ -0,0 +1,33 @@
+﻿using SCAS.Module;
+
+namespace SCAS.Domain.UserContext
+{
+    public struct PersonDeletedEventData
+    {
+        public string ID { get; }
+
+        internal PersonDeletedEventData(Person person)
+        {
+            ID = person.ID;
+        }
+    }
+
+    public class PersonDeletedEvent
+        : UserContextArtificialEventBase<DomainEventValue, PersonDeletedEventData>
+    {
+        private Person person;
+
+        public override string Message => string.Format("Person {0} was deleted.", person.Info.Name);
+
+        internal PersonDeletedEvent(Person op, Person targetPerson, IExtractor extractor = null)
+            : base(op, UserContextEvent.OrganizationArchived, SCASEventType.Model, SCASEventLevel.Common, SCASEventPriority.Common, new PersonDeletedEventData(targetPerson), extractor)
+        {
+            person = targetPerson;
+        }
+
+        public override DomainEventValue ToValue()
+        {
+            return base.ToValue(new DomainEventValue { });
+        }
+    }
+}
